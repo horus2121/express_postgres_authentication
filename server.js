2e9a39387293d0386c89f4d4840f7e5b502d7fe5
Horus Hong <horus@192-168-1-105.tpgi.com.au>
2023-02-03 23:43:17 +1100

    test auth routes

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const cors = require("cors");
+const cookieSession = require('cookie-session');
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+const db = require("./app/models");
+const { cookie } = require("express/lib/response");
+
+db.sequelize.sync()
+  .then(() => {
+    console.log("Synced db.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+    cookieSession({
+        name: "auth-session",
+        secret: "COOKIE_SECRET",
+        httpOnly: true
+    })
+);
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+// routes
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
